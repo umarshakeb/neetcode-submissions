7366c32f9972d2c1d55de0648234a08ebbbb3938
Shakeb <68895910+umarshakeb@users.noreply.github.com>
2026-07-02 21:34:20 +0530

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0 or (x != 0 and x%10==0):
+            return False
+        reverse_x = 0
+        while x > reverse_x:
+            reverse_x = reverse_x*10 + x%10
+            x //= 10
+        return (x==reverse_x) or (x==reverse_x//10) 
+
+
+        
