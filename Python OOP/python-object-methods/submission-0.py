ddfe0619fedafc6a896e66c2250020a4ed4b6731
Shakeb <68895910+umarshakeb@users.noreply.github.com>
2026-07-19 17:29:05 +0530

    Add: python-object-methods - submission-0

---
+++ Python OOP/python-object-methods/submission-0.py
@@ -0,0 +1,19 @@
+class Pet:
+    def __init__(self, name: str):
+        self.name = name
+        self.hunger = 5
+
+    def feed(self):
+        # TODO: Implement this method
+        # It should decrease the pet's hunger by 1
+        # and print a message about feeding the pet
+        self.hunger -= 1
+        print(f"Fluffy has been fed.")
+        print(f"Fluffy's hunger level: {self.hunger}")
+
+# Create a pet
+my_pet = Pet("Fluffy")
+
+# TODO: Feed the pet three times
+for i in range(3):
+    my_pet.feed()
